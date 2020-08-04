706eee6685436cd8a13dd7b03b837e90f43c4207
jiyanglee <dlwldid2@nate.com>
2020-08-04 15:45:07 +0900

    crawling some web sites

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import math
+import requests as rq
+from bs4 import BeautifulSoup
+
+indeed_result=rq.get("https://kr.indeed.com/jobs?q=python&l=%EA%B2%BD%EA%B8%B0%EB%8F%84+%EA%B3%A0%EC%96%91")
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+ 
+pagination= indeed_soup.find("div",{"class":"pagination"})
+
+pages= pagination.find_all('a')
+
+for page in pages:
+    print(page.find('span'))
