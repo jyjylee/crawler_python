ea0ac7392a087ad993632c89c1028dad9e733239
jiyanglee <dlwldid2@nate.com>
2020-08-04 15:48:29 +0900

    #2.3 Extracting Indeed Pages (10:20)

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 pagination= indeed_soup.find("div",{"class":"pagination"})
 
 pages= pagination.find_all('a')
-
+spans=[]
 for page in pages:
-    print(page.find('span'))
+    spans.appen(page.find("span"))
+spans=spans[:-1]
